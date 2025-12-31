29e15a54e451434ca83efc939758b0def963b274
JothSh <jotheeshit@gmail.com>
2025-12-31 16:34:42 +0530

    Learning static methods

--- src/main/java/methodsConcepts/LearningStaticMethods.java
+++ src/main/java/methodsConcepts/LearningStaticMethods.java
@@ -2,4 +2,31 @@ package methodsConcepts;
 
 public class LearningStaticMethods {
 
+	public static void function() {
+		
+		System.out.println("Hello from non static method");
+		
+		
+		
+	}
+	
+	public static void main(String[]args) {
+		
+		function();
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
