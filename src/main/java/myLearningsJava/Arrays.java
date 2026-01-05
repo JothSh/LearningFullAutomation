0b49a14485097c4cacba9328e837227c284f284f
JothSh <jotheeshit@gmail.com>
2026-01-05 17:03:24 +0530

    Arrays

---
+++ src/main/java/myLearningsJava/Arrays.java
@@ -0,0 +1,17 @@
+package myLearningsJava;
+
+public class Arrays {
+
+	
+	
+	public static void main (String [] args) {
+		
+		String [] cards = {"Ace","King","Queen","Jack"};
+		
+		System.out.println(cards[1]);
+		System.out.println(cards.length);
+		
+	}
+	
+	
+}
